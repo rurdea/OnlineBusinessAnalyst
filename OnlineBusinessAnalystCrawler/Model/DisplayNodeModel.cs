567288587aa8f936a5225d5dbca4bf2534aa44a5
rurdea <radu.urdea@hotmail.com>
2015-03-21 12:25:07 +0200

    added missing model;

---
+++ OnlineBusinessAnalystCrawler/Model/DisplayNodeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineBusinessAnalystCrawler.Model
+{
+    public class DisplayNodeModel
+    {
+        public int Index { get; set; }
+        public string ParentUrl { get; set; }
+        public string Url { get; set; }
+        public int Progress { get; set; }
+    }
+}
